41c1a1187acad775c98b168396e8f31412b92b33
J. Japa <jjv@live.dk>
2025-04-10 19:10:32 +0200

    Added temporary index.jsx file for postings

---
+++ frontend/src/scenes/form/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Form = () => {
+    return <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="FORM" subtitle="Welcome to your form"/>
+        </Box>
+    </Box>
+}
+
+export default Form;
