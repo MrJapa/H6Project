5af520f8aaf792f9b384829723ff8c3047a78108
J. Japa <jjv@live.dk>
2025-04-14 15:35:26 +0200

    Added login view

--- backend/SafeLedger/urls.py
+++ backend/SafeLedger/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostingsListView
+from .views import PostingsListView, login_view
 
 urlpatterns = [
     path('postings/', PostingsListView.as_view(), name='postings-list'),  # URL for the postings list view
+    path('login/', login_view, name='login'),  # URL for the login view
 ]
