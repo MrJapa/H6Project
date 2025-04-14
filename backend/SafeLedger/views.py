c7026088619849973397e99ab3ed4cdbe5b4c2a6
J. Japa <jjv@live.dk>
2025-04-14 15:29:03 +0200

    New API user view added

--- backend/SafeLedger/views.py
+++ backend/SafeLedger/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import User, Postings, Company
 from .serializers import PostingsSerializer
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 class PostingsListView(generics.ListCreateAPIView):
     queryset = Postings.objects.all()
     serializer_class = PostingsSerializer
+
+@api_view(['POST'])
+def login_view(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 # Create your views here.
