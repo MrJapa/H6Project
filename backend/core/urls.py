2add4024e4f5a7a513f6189c78e7786017e68dfb
J. Japa <jjv@live.dk>
2025-04-24 17:35:10 +0200

    added repath

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from SafeLedger.views import FrontendAppView
 
 
 urlpatterns = [
     path('api/', include('SafeLedger.urls')),  # Include the URLs from the SafeLedger app
     path('admin/', admin.site.urls),
+    re_path(r'^.*$', FrontendAppView.as_view()),  # Catch-all URL for the frontend app
 ]
